2d3955a69dd9b55bd474de5790a40452788b442b
Phillip Denness <dennessphillip@gmail.com>
2019-01-16 17:37:46 +0000

    sync package lock as requested by travis

--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,7 @@ const router = express.Router()
 
 const authInterceptor = require('./lib/middleware/auth')
 const serviceTokenMiddleware = require('./lib/middleware/service-token')
+const documentsRoutes = require('./controllers/documents')
 
 const dmStoreApiRoutes = require('./services/dm-store-api/dm-store-api')
 const emAnnoApiRoutes = require('./services/em-anno-api/em-anno-api')
@@ -18,10 +19,12 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 router.use(serviceTokenMiddleware)
 auth(router)
 router.use(authInterceptor)
+documentsRoutes(router)
 
 if (config.configEnv !== 'prod') {
     // Uncomment to enable direct access to Microservices
     dmStoreApiRoutes(router)
+
     idamApiRoutes(router)
     s2sApiRoutes(router)
 }
