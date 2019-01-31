e2dfa496aac6a14d2fad38bce11a7d3babe2ec71
taleb <taleb.benouaer@hmcts.net>
2019-01-31 19:29:17 +0000

    Add styling to the demo app

--- projects/annotation-ui-app/src/app/annotation-webapp/annotation-webapp.component.ts
+++ projects/annotation-ui-app/src/app/annotation-webapp/annotation-webapp.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-annotation-webapp',
     templateUrl: './annotation-webapp.component.html',
-    styleUrls: []
+    styleUrls: ['./annotation-webapp.component.scss']
 })
 export class AnnotationWebappComponent {
     constructor() {
--- projects/annotation-ui-app/src/app/app.component.ts
+++ projects/annotation-ui-app/src/app/app.component.ts
@@ -8,38 +8,8 @@ import { AuthService } from './auth/auth.service';
     styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
-    title = 'EM Annotation WebApp';
-    logoutLink: string;
+export class AppComponent {
+    title = 'Annotation Library Demo App';
 
-    constructor(private router: Router, private authService: AuthService) {
-        this.logoutLink = `/logout?redirect=${encodeURIComponent(this.authService.generateLoginUrl())}`;
-    }
-
-    ngOnInit() {
-        this.router.events.subscribe((event: Event) => {
-            if (event instanceof NavigationEnd) {
-                const replacedTitles = this.replacedTitles(event.url);
-                this.title = this.getTitle(replacedTitles);
-            }
-        });
-    }
-
-    private replacedTitles(url: string): string {
-        if (url.indexOf('summary') !== -1) {
-            return 'summary';
-        }
-        if (url.indexOf('parties') !== -1) {
-            return 'parties';
-        }
-        return '/';
-    }
-
-    private getTitle(key): string {
-       const titleMapping: {[id: string]: string} = {
-           '/' : this.title
-       };
-
-       return titleMapping[key];
-    }
+    constructor() {};
 }
