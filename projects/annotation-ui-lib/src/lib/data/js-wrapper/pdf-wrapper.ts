e4d80474e97cf856db520dd0179aa94254cadb2a
taleb <taleb.benouaer@hmcts.net>
2019-02-06 12:23:52 +0000

    Use combined pdfjs file to remove need for setting worker path

--- projects/annotation-ui-lib/src/lib/data/js-wrapper/pdf-wrapper.ts
+++ projects/annotation-ui-lib/src/lib/data/js-wrapper/pdf-wrapper.ts
@@ -2,10 +2,6 @@ declare const PDFJS: any;
 
 export class PdfWrapper {
 
-    constructor() {
-        PDFJS.workerSrc = '/assets/js/pdf.worker.min.js';
-    }
-
     getDocument(documentId): Promise<any> {
         return PDFJS.getDocument(documentId);
     }
